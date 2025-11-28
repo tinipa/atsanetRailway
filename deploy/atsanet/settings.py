22d7288b70c47706c2daf59b0ca67acca3c718fc
MARTIN ANDRES ACEVEDO VARGAS <tin@live.com.ar>
2025-11-27 23:22:45 -0500

    conex

--- deploy/atsanet/settings.py
+++ deploy/atsanet/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-s%+imnlby6f$tz+%#f7%%-k6x(5k(n#730v_lg6#ibneziqb4+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
